1fd77efb82f0d757780165799193fcc5d94790af
zzi <zzi@wolf.gg>
2018-04-10 14:48:05 -0500

    make sure navigator is not null

--- SidestepPlugin.cs
+++ SidestepPlugin.cs
@@ -11,8 +11,10 @@ using ff14bot.Behavior;
 using ff14bot.Enums;
 using ff14bot.Helpers;
 using ff14bot.Managers;
+using ff14bot.Navigation;
 using ff14bot.Objects;
 using ff14bot.Overlay3D;
+using ff14bot.Pathing;
 using ff14bot.Pathing.Avoidance;
 using Sidestep.Helpers;
 using Sidestep.Interfaces;
@@ -118,6 +120,10 @@ namespace Sidestep
 
 	    public override void OnPulse()
 	    {
+            //don't run if we don't have a navigation provider
+            if (Navigator.NavigationProvider == null)
+                return;
+
 	        using (new PerformanceLogger("Pulse"))
 	        {
                 //remove tracked avoidances that have completed
