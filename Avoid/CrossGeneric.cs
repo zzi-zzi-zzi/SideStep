e4d7952179c03834e05dd2d32b8d076511e6058b
nt153133 <52430037+nt153133@users.noreply.github.com>
2019-10-26 20:08:59 -0400

    forgot license

--- Avoid/CrossGeneric.cs
+++ Avoid/CrossGeneric.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+SideStep is licensed under a
+Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+You should have received a copy of the license along with this
+work. If not, see <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
+Orginal work done by zzi
+                                                                                 */
+
+using System;
 using System.Collections.Generic;
 using ff14bot.Managers;
 using ff14bot.Objects;
