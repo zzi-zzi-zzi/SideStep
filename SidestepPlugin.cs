924bccbc1939fb0ab47a056704dee37754630385
mastahg <mastahg@users.noreply.github.com>
2019-11-06 01:27:28 -0500

    Optimize and cleanup the main plugin logic

--- SidestepPlugin.cs
+++ SidestepPlugin.cs
@@ -132,7 +132,7 @@ namespace Sidestep
 
 	    
 
-	    private List <AvoidInfo> _tracked = new List<AvoidInfo>();
+	    private readonly List <AvoidInfo> _tracked = new List<AvoidInfo>();
 
 	    public override void OnPulse()
 	    {
@@ -143,46 +143,20 @@ namespace Sidestep
 	        using (new PerformanceLogger("Pulse"))
 	        {
                 //remove tracked avoidances that have completed
-	            var removeable = _tracked.Where(i => !i.Condition() && !AvoidanceManager.Avoids.Any(z => z.AvoidInfo == i)).ToList();
+	            var removable = _tracked.Where(i => !i.Condition() && AvoidanceManager.Avoids.All(z => z.AvoidInfo != i)).ToList();
 
-                if (removeable.Any())
+                if (removable.Any())
 	            {
-	                Logger.Info($"Removing {removeable.Count()} completed spells");
-	                AvoidanceManager.RemoveAllAvoids(i => removeable.Contains(i));
-	                _tracked.RemoveAll(x => removeable.Contains(x));
+	                Logger.Info($"Removing {removable.Count} completed spells");
+	                AvoidanceManager.RemoveAllAvoids(i => removable.Contains(i));
+	                _tracked.RemoveAll(x => removable.Contains(x));
 	            }
+
+
 	            var newSpellCasts = GameObjectManager.GetObjectsOfType<BattleCharacter>()
-	                .Where(c => c.InCombat && !c.IsMe && c.StatusFlags.HasFlag(StatusFlags.Hostile) && c.CastingSpellId != 0 && IsNearby(c) &&
-                                
-                                c.SpellCastInfo.SpellData.Omen != 0 && //skip spells that don't have an omen
-
-	                            !AvoidanceManager.AvoidInfos.Any(s => s.Collection.Contains(c)))
-	                            .GroupBy(p => p.ObjectId)
-                    .SelectMany(o =>
-	                {
-	                    var c = o.First();
-	                    if (_owners.ContainsKey((ulong) AvoiderType.Spell + c.CastingSpellId))
-	                    {
-	                        Logger.Verbose($"{c.SpellCastInfo.SpellData.LocalizedName} [Spell][Id: {c.CastingSpellId}][Omen: {c.SpellCastInfo.SpellData.Omen}][RawCastType: {c.SpellCastInfo.SpellData.RawCastType}][ObjId: {c.ObjectId}]");
-                            return _owners[(ulong) AvoiderType.Spell + c.CastingSpellId].Handle(c);
-	                    }
-
-	                    if (_owners.ContainsKey((ulong) AvoiderType.Omen + c.SpellCastInfo.SpellData.Omen))
-	                    {
-	                        Logger.Verbose($"{c.SpellCastInfo.SpellData.LocalizedName} [Omen][Id: {c.CastingSpellId}][Omen: {c.SpellCastInfo.SpellData.Omen}][RawCastType: {c.SpellCastInfo.SpellData.RawCastType}][ObjId: {c.ObjectId}]");
-                            return _owners[(ulong) AvoiderType.Omen + c.SpellCastInfo.SpellData.Omen].Handle(c);
-	                    }
-
-	                    if (_owners.ContainsKey((ulong) AvoiderType.CastType + c.SpellCastInfo.SpellData.RawCastType))
-	                    {
-	                        Logger.Verbose($"{c.SpellCastInfo.SpellData.LocalizedName} [CastType][Id: {c.CastingSpellId}][Omen: {c.SpellCastInfo.SpellData.Omen}][RawCastType: {c.SpellCastInfo.SpellData.RawCastType}][ObjId: {c.ObjectId}]");
-                            return _owners[(ulong) AvoiderType.CastType + c.SpellCastInfo.SpellData.RawCastType].Handle(c);
-	                    }
-
-	                    Logger.Verbose($"No Avoid info for: {c.SpellCastInfo.SpellData.LocalizedName} [None][Id: {c.CastingSpellId}][Omen: {c.SpellCastInfo.SpellData.Omen}][RawCastType: {c.SpellCastInfo.SpellData.RawCastType}][ObjId: {c.ObjectId}]");
-                        return null;
-	                })
-                    .Where(i => i != null).ToList();
+                .Where(IsValid)
+                .SelectMany(HandleNewCast)
+                .Where(i => i != null).ToList();
 
 	            if (newSpellCasts.Any())
 	            {
@@ -193,10 +167,63 @@ namespace Sidestep
 	        }
 	    }
 
-	  
-	    private static bool IsNearby(GameObject character)
-	    {
-	        return character.Distance() < 50;
-	    }
+        private IEnumerable<AvoidInfo> HandleNewCast(BattleCharacter battleCharacter)
+        {
+            if (_owners.TryGetValue((ulong) AvoiderType.Spell + battleCharacter.CastingSpellId,out var iAvoider))
+            {
+                Logger.Verbose($"{battleCharacter.SpellCastInfo.SpellData.LocalizedName} [Spell][Id: {battleCharacter.CastingSpellId}][Omen: {battleCharacter.SpellCastInfo.SpellData.Omen}][RawCastType: {battleCharacter.SpellCastInfo.SpellData.RawCastType}][ObjId: {battleCharacter.ObjectId}]");
+            }
+            else if (_owners.TryGetValue((ulong) AvoiderType.Omen + battleCharacter.SpellCastInfo.SpellData.Omen, out iAvoider))
+            {
+                Logger.Verbose($"{battleCharacter.SpellCastInfo.SpellData.LocalizedName} [Omen][Id: {battleCharacter.CastingSpellId}][Omen: {battleCharacter.SpellCastInfo.SpellData.Omen}][RawCastType: {battleCharacter.SpellCastInfo.SpellData.RawCastType}][ObjId: {battleCharacter.ObjectId}]");
+            }
+            else if (_owners.TryGetValue((ulong) AvoiderType.CastType + battleCharacter.SpellCastInfo.SpellData.RawCastType, out iAvoider))
+            {
+                Logger.Verbose($"{battleCharacter.SpellCastInfo.SpellData.LocalizedName} [CastType][Id: {battleCharacter.CastingSpellId}][Omen: {battleCharacter.SpellCastInfo.SpellData.Omen}][RawCastType: {battleCharacter.SpellCastInfo.SpellData.RawCastType}][ObjId: {battleCharacter.ObjectId}]");
+            }
+            else
+            {
+                Logger.Verbose($"No Avoid info for: {battleCharacter.SpellCastInfo.SpellData.LocalizedName} [None][Id: {battleCharacter.CastingSpellId}][Omen: {battleCharacter.SpellCastInfo.SpellData.Omen}][RawCastType: {battleCharacter.SpellCastInfo.SpellData.RawCastType}][ObjId: {battleCharacter.ObjectId}]");
+                return new AvoidInfo[0];
+            }
+
+            return iAvoider.Handle(battleCharacter);
+        }
+
+
+        private static bool IsValid(BattleCharacter c)
+        {
+
+            if (!c.InCombat)
+                return false;
+
+            if (c.IsMe)
+                return false;
+
+            if (!c.StatusFlags.HasFlag(StatusFlags.Hostile))
+                return false;
+
+            if (c.CastingSpellId == 0)
+                return false;
+
+
+            if (c.DistanceSqr() < 50 * 50)
+            {
+                if (c.SpellCastInfo.SpellData.Omen != 0)
+                {
+                    if (!AvoidanceManager.AvoidInfos.Any(s => s.Collection.Contains(c)))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+
+            //var valid = c.InCombat && !c.IsMe && c.StatusFlags.HasFlag(StatusFlags.Hostile) && c.CastingSpellId != 0 &&
+            //            c.SpellCastInfo.SpellData.Omen != 0 && //skip spells that don't have an omen
+            //
+            //            !AvoidanceManager.AvoidInfos.Any(s => s.Collection.Contains(c));
+            return false;
+        }
     }
 }
